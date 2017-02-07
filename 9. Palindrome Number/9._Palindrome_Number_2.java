edd3eb2f2a817491a0d991123109ede18a0972a4
Ming-Lung Chen <tyge318@gmail.com>
2017-02-07 14:39:56 -0800

    Add .trace file

--- 9. Palindrome Number/9._Palindrome_Number_2.java
+++ 9. Palindrome Number/9._Palindrome_Number_2.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0 )
+        if( x < 0 )
             return false;
-        ArrayList<Integer> digits = new ArrayList<Integer>();
-        while( x > 0 ) {
-            digits.add( x % 10 );
-            x /= 10;
+        int p = x, q = 0;
+        
+        while( p >= 10) {
+            q *= 10;
+            q += p % 10;
+            p /= 10;
         }
-        int length = digits.size();
-        int left = 0, right = length-1;
-        while( left < right ) {
-            if( digits.get(left) != digits.get(right) )
-                return false;
-            left++;
-            right--;
-        }
-        return true;
+        return q == x/10 && p == x % 10;
     }
 }
